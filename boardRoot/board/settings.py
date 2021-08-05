8c4d5bf2470aa7c8dc1ebf6c2d452d7f0b4e73fc
kimjjing1004 <kimjjing1004@naver.com>
2021-08-05 18:13:10 +0900

    update file

--- boardRoot/board/settings.py
+++ boardRoot/board/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import pymysql
+from pathlib import Path
+
+import pymysql
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
